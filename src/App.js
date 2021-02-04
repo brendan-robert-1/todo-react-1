18515082943892b0cf96992b14817cbd722dc80e
Brendan Robert <brendan.robert50point@gmail.com>
2021-02-04 00:36:42 -0500

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import TodoList from './TodoList';
+import TodoButton from './TodoButton';
+import TodoInput from './TodoInput';
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      todo:'',
+      todos:[]
+    }
+  }
   render(){
     return (
       <div className="App">
-          
+        <TodoInput
+          type='text'
+          placeholder='Enter new task...'
+        />
+        <TodoButton
+        />
+        <TodoList/>
       </div>
     );
   }
